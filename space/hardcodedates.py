02fa72c403d84185d34c25d9bfed460b8d0ffcb1
ryanb <ryanb@28b174d2-27d0-0310-a1ca-f369082887e2>
2006-12-06 20:51:43 +0000

    update to 1.3

--- space/hardcodedates.py
+++ space/hardcodedates.py
@@ -12,23 +12,26 @@ Example of a line for the file /var/data-dir/school/abc.txt
 
 2004-08-09-00-00 school/abc.txt
 """
-__author__ = 'Nathan Kent Bullock'
-__homepage__ = 'http://bullock.moo.com/nathan/'
-__email__ = 'nathan_kent_bullock -at- yahoo.ca'
-__version__ = '1.2'
+__author__ = 'Nathan Kent Bullock, Ryan Barrett'
+__homepage__ = 'http://snarfed.org/space/hardcodedates'
+__email__ = 'nathan_kent_bullock -at- yahoo.ca, hardcodedates -at- ryanb.org'
+__version__ = '1.3'
 
 from Pyblosxom import tools
 import os, os.path, re, time
 
 FILETIME = re.compile('^([0-9]{4})-([0-1][0-9])-([0-3][0-9])-([0-2][0-9])-([0-5][0-9]) +(.*)$')
 
-all_timestamps = None
-extensions = None
+all_timestamps = {}
+extensions = []
 timestamps_to_save = {}
 
-def get_all_timestamps(datadir):
+def init(request):
+	if all_timestamps:
+		return  # already initialized
+
+	datadir = request.getConfiguration()['datadir']
 	f = open(datadir + "/timestamps")
-	t = {}
 	for str in f.readlines():
 		m = FILETIME.search(str.strip())
 		if m:
@@ -38,17 +41,17 @@ def get_all_timestamps(datadir):
 			hr = int(m.group(4))
 			minute = int(m.group(5))
 			mtime = time.mktime((year,mo,day,hr,minute,0,0,0,-1))
-			
-			t[datadir + "/" + m.group(6)] = mtime
+			all_timestamps[datadir + "/" + m.group(6)] = mtime
 
 	f.close()
-	return t
+
+	extensions.extend(request.getData()['extensions'].keys())
+	extensions.append(request.getConfiguration().get('comment_ext', ''))
 
 def cb_filestat(args):
-	global all_timestamps
+	init(args['request'])
 
-	filename = args["filename"]
-
+	filename = args['filename']
 	if all_timestamps.has_key(filename):
 		# we know this file's timestamp
 		args['mtime'][8] = all_timestamps[filename]
@@ -61,18 +64,9 @@ def cb_filestat(args):
 
 	return args
 
-def cb_start(args):
-	global all_timestamps, extensions
-	request = args['request']
-
-	all_timestamps = get_all_timestamps(args["request"].getConfiguration()['datadir'])
-
-	extensions = request.getData()['extensions'].keys()
-	extensions.append(request.getConfiguration().get('comment_ext', ''))
-
 def cb_end(args):
 	if timestamps_to_save:
-		datadir = args["request"].getConfiguration()['datadir']
+		datadir = args['request'].getConfiguration()['datadir']
 		tsfile = file(datadir + "/timestamps", 'a')
 		for filename, mtime in timestamps_to_save.items():
 			time_str = time.strftime('%Y-%m-%d-%H-%M', time.localtime(mtime))
