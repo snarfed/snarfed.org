caac9a442ab89f00482e43cf46c0044616f7d550
ryanb <ryanb@28b174d2-27d0-0310-a1ca-f369082887e2>
2007-08-26 01:15:45 +0000

    update to handle simon's new form parameters. also add more error handling and comments.

--- space/simonitor.sh
+++ space/simonitor.sh
@@ -9,6 +9,12 @@
 # so the user is shown the CAPTCHA images and asked to enter its text.
 #
 # Usage: simonitor.sh CARDNUMBER/CCV [CARDNUMBER/CCV ...]
+#
+#
+# I've used the Live HTTP Headers Firefox plugin to help debug and update
+# recent versions of this when Simon changes their web site. It's a huge help:
+#
+# http://livehttpheaders.mozdev.org/
 
 if [ $# == 0 -o "$1" == "-h" -o "$1" == "--help" ]; then
   echo "Fetches the current balance of one or more Simon gift cards."
@@ -21,40 +27,56 @@ fi
 # evidently it's a base64-encoded struct. it doesn't look like it's actually
 # used for anything, but it still needs to be there. note that %2B is the url
 # encoding for +.
-VIEWSTATE=dDwtMTQ3MDgxMDk2Mjt0PDtsPGk8MT47aTwzPjtpPDU%2BO2k8OT47PjtsPHQ8O2w8aTwwPjs%2BO2w8dDxwPGw8VGV4dDs%2BO2w8U2ltb24gTWFsbHMgfCBNb3JlIENob2ljZXMgLSBUaGUgUHJlcGFpZCBWaXNhIGRlYml0IEdpZnQgQ2FyZCBhbmQgVmlzYSBHaWZ0IEFjY291bnQgZnJvbSBTaW1vbi5jb207Pj47Oz47Pj47dDw7bDxpPDA%2BOz47bDx0PDtsPGk8MD47PjtsPHQ8cDxsPFRleHQ7PjtsPC87Pj47Oz47Pj47Pj47dDw7bDxpPDE%2BO2k8Mz47aTw1Pjs%2BO2w8dDw7bDxpPDA%2BO2k8MT47PjtsPHQ8cDxsPFZpc2libGU7PjtsPG88dD47Pj47bDxpPDA%2BOz47bDx0PHA8cDxsPFNlbGVjdGVkTm9kZUlkO0VuYWJsZVZpZXdTdGF0ZTs%2BO2w8cDE5O288dD47Pj47Pjs7Pjs%2BPjt0PHA8bDxWaXNpYmxlOz47bDxvPGY%2BOz4%2BO2w8aTwwPjs%2BO2w8dDxwPHA8bDxTZWxlY3RlZE5vZGVJZDtFbmFibGVWaWV3U3RhdGU7PjtsPHBFO288dD47Pj47Pjs7Pjs%2BPjs%2BPjt0PDtsPGk8MT47aTwzPjtpPDIxPjtpPDI3Pjs%2BO2w8dDxwPGw8VmlzaWJsZTs%2BO2w8bzx0Pjs%2BPjs7Pjt0PHA8bDxWaXNpYmxlOz47bDxvPHQ%2BOz4%2BOzs%2BO3Q8cDw7cDxsPGF1dG9jb21wbGV0ZTs%2BO2w8b2ZmOz4%2BPjs7Pjt0PHA8cDxsPENhcHRjaGFJbWFnZV9DUENvbnRyb2xJRDtJbWFnZVVybDs%2BO2w8dHh0Q2FwdGNoYTtyZWNpcGllbnQvaW1hZ2VzL2NhcHRjaGEvNDExNS5qcGc7Pj47Pjs7Pjs%2BPjt0PHA8cDxsPFZpc2libGU7PjtsPG88Zj47Pj47PjtsPGk8MTU%2BO2k8MTc%2BOz47bDx0PHQ8OztsPGk8MD47Pj47Oz47dDxAMDw7Ozs7Ozs7Ozs7Pjs7Pjs%2BPjs%2BPjt0PDtsPGk8MD47aTwxPjtpPDI%2BOz47bDx0PHA8cDxsPE5hdmlnYXRlVXJsOz47bDwvUHJpdmFjeVBvbGljeS5hc3B4Oz4%2BOz47Oz47dDxwPHA8bDxOYXZpZ2F0ZVVybDs%2BO2w8L2ZpbmRhbWFsbDs%2BPjs%2BOzs%2BO3Q8cDxwPGw8TmF2aWdhdGVVcmw7PjtsPC9hYm91dF9zaW1vbi9jb250YWN0X3NwZzs%2BPjs%2BOzs%2BOz4%2BOz4%2BO2w8SGVhZGVyMTpUb3BOYXZpZ2F0aW9uMTp0b3BNZW51O2J0blN1Ym1pdDs%2BPvMIfy36JAYwb0tY8M0PgAWHOg1W
+#
+# when Simon changes their web site, this usually needs to be updated.
+VIEWSTATE=%2FwEPDwUJNTc2OTY4MDgyD2QWAmYPZBYCAgMPZBYGAgEPZBYCZg9kFgJmDxYCHgRUZXh0BQEvZAIDDxYCHgxhdXRvY29tcGxldGUFA29mZhYCAgEPZBYGAgEPZBYEZg8WAh4HVmlzaWJsZWcWAmYPDxYCHg9FbmFibGVWaWV3U3RhdGVnZGQCAQ8WAh8CaBYCZg8PFgIfA2dkZAIDD2QWCAIBDxYCHwJnZAIDDxYCHwJnZAIVDw9kFgIfAQUDb2ZmZAIbDw8WBB4YQ2FwdGNoYUltYWdlX0NQQ29udHJvbElEBSRjdGwwMCRDb250ZW50UGxhY2VIb2xkZXIxJHR4dENhcHRjaGEeCEltYWdlVXJsBSVyZWNpcGllbnQvaW1hZ2VzL2NhcHRjaGEvNzg0OTMzNDAuanBnZGQCBQ8PFgIfAmhkFgQCDw8QZGQWAWZkAhEPPCsACwBkAgUPZBYGZg8PFgIeC05hdmlnYXRlVXJsBRMvUHJpdmFjeVBvbGljeS5hc3B4ZGQCAQ8PFgIfBgUKL2ZpbmRhbWFsbGRkAgIPDxYCHwYFGC9hYm91dF9zaW1vbi9jb250YWN0X3NwZ2RkGAEFHl9fQ29udHJvbHNSZXF1aXJlUG9zdEJhY2tLZXlfXxYCBSRjdGwwMCRIZWFkZXIxJFRvcE5hdmlnYXRpb24xJHRvcE1lbnUFI2N0bDAwJENvbnRlbnRQbGFjZUhvbGRlcjEkYnRuU3VibWl0nq6rj%2FdiWrU1I0kobPTuvp1TDV0%3D
 
 COOKIE_FILE=/tmp/simonitor_cookie.jar
 CURL_ARGS="--cookie-jar $COOKIE_FILE --cookie $COOKIE_FILE --silent --show-error --insecure"
+XLOADIMAGE_PIDS=""
 
-for cc in $*; do
+for CC in $*; do
   rm -f $COOKIE_FILE
-  CC_ARGS="ccNumberBalance=`echo $cc | sed 's/\//\&ccCid=/'`"
+
+  # parse credit card number and CCV
+  CC_NUM=`echo $CC | grep -o -e '^[0-9]\{16\}'`
+  CC_CID=`echo $CC | grep -o -e '[0-9]\{3\}$'`
+  if [[ $CC_NUM == "" || $CC_CID == "" ]]; then
+    echo "Invalid card number or CCV: $CC"
+    continue
+  fi
+
 
   # GET the form page and the captcha image
   CAPTCHA=`curl $CURL_ARGS https://www.simon.com/giftcard/card_balance.aspx | \
              egrep -o 'recipient/images/captcha/[^.]+\.jpg'`
   CAPTCHAFILE=`mktemp /tmp/simonitor_captcha.XXXXXX` || exit 1
   curl --output $CAPTCHAFILE $CURL_ARGS https://www.simon.com/giftcard/$CAPTCHA
-  xloadimage $CAPTCHAFILE &> /dev/null || exit 1 &
+  xloadimage $CAPTCHAFILE > /dev/null &
+  XLOADIMAGE_PID="$XLOADIMAGE_PID $!"
 
-  # ask for the captcha
-  read -e -p "Enter the captcha string: " captcha
+  # ask for the captcha.
+  #
+  # sometimes their captcha generator breaks and only generates AAAAAA. when
+  # that happens, i comment out the read command and replace it with this line.
+#   CAPTCHA="aaaaaa"
+  read -e -p "Enter the captcha string: " CAPTCHA
 
   # erase the prompt w/ANSI escape codes. [1A moves the cursor up a line, [0K
   # erases the line. see http://www.answers.com/topic/ansi-escape-code .
   echo -ne '\e[1A\e[0K'
 
-  # POST the form data
-  if ( ! curl $CURL_ARGS --output /dev/null \
-         --data "$CC_ARGS&__VIEWSTATE=$VIEWSTATE&btnSubmit.x=0&btnSubmit.y=0&txtCaptcha=$captcha" \
+  # POST the form data. note that %24 is the url encoding for the $ character.
+  if ( ! curl --output /dev/null $CURL_ARGS \
+         --data "__VIEWSTATE=$VIEWSTATE&ctl00%24ContentPlaceHolder1%24btnSubmit.x=0&ctl00%24ContentPlaceHolder1%24btnSubmit.y=0&ctl00%24ContentPlaceHolder1%24txtCaptcha=$CAPTCHA&ctl00%24ContentPlaceHolder1%24ccNumberBalance=$CC_NUM&ctl00%24ContentPlaceHolder1%24ccCid=$CC_CID" \
          https://www.simon.com/giftcard/card_balance.aspx ); then
-    exit
+    continue
   fi
 
   # handle the redirect. iirc, this request sets the auth cookie we need.
   if ( ! curl $CURL_ARGS --output /dev/null \
          https://www.simon.com/giftcard/process.ashx ); then
-    exit
+    continue
   fi
 
   # GET the balance page and scrape the address, expiration date, and balance
@@ -63,13 +85,18 @@ for cc in $*; do
 
   BALANCE=`egrep -o 'lblBalance"><b>[^<]+' $TMPFILE | sed 's/lblBalance"><b>//'`
   EXPDATE=`egrep -o 'lblExpDate">[^<]+' $TMPFILE | sed 's/lblExpDate">//'`
-  echo "$cc: $BALANCE, expires $EXPDATE"
+  echo "$CC: $BALANCE, expires $EXPDATE"
 
+  NAME=`egrep -o 'lblPersonName">[^<]*' $TMPFILE | sed 's/lblPersonName">//'`
   CITY_STATE_ZIP=`egrep -o 'lblPersonAddress">[^<]*<br>[^<]+' $TMPFILE \
-                  | sed 's/lblPersonAddress">[^<]*<br>//'`
-  if [ "$CITY_STATE_ZIP" != "" ]; then
-    echo "                      $CITY_STATE_ZIP"
+                  | sed 's/lblPersonAddress">//' | sed 's/<br>/ /'`
+  if [ "$NAME $CITY_STATE_ZIP" != " " ]; then
+    echo "                      $NAME  $CITY_STATE_ZIP"
   fi
 
-#   shred -u $TMPFILE
+  shred -u $TMPFILE $CAPTCHAFILE
 done
+
+if [[ $XLOADIMAGE_PID != "" ]]; then
+  kill $XLOADIMAGE_PID
+fi
