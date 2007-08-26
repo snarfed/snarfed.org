95d93bd3f4e0413e84bb7389fd07a67aeb373185
ryanb <ryanb@28b174d2-27d0-0310-a1ca-f369082887e2>
2007-08-26 18:55:10 +0000

    fix formatting, switch from wget to curl

--- space/download_picasa_album.sh
+++ space/download_picasa_album.sh
@@ -11,10 +11,10 @@
 #
 # The script itself is dirt simple. If you're looking for something robust and
 # full-featured, you probably want an RSS reader that supports enclosures or a
-#dedicated web photo downloader instead. This is simpler, though, which I like.
+# dedicated web photo downloader instead. This is simpler, though, which I like.
 
 curl $1 | \
   sed "y/</\n/" | \
   grep -o -e "media:content url='[^']\+" | \
   sed "s/media:content url='//" | \
-  xargs wget
+  xargs -n 1 curl -O
